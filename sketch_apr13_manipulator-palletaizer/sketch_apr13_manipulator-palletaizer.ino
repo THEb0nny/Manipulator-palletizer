b372e6425cde0e2682ebcd32614da91b8fceab77
Dmitriy Antipov <aston_bonny@mail.ru>
2023-05-19 19:43:53 +0300

    19_05_23_2

--- sketch_apr13_manipulator-palletaizer/sketch_apr13_manipulator-palletaizer.ino
+++ sketch_apr13_manipulator-palletaizer/sketch_apr13_manipulator-palletaizer.ino
@@ -11,39 +11,35 @@
 
 #define SERVO_MIN_PULSE 500
 #define SERVO_MAX_PULSE 2500
+#define SERVO_START_POS 90
 
-ServoSmooth servos[SERVO_AMOUNT];
+ServoSmooth servos[SERVO_AMOUNT](360); // Создаём объекты серв с указанием, что максимальный угол 360
 
 TimerMs tmr(1000, 0, 0);
+TimerMs tmrDebug(200, 1, 0);
 
 int servosPins[SERVO_AMOUNT] = {SERVO1_PIN, SERVO2_PIN, SERVO3_PIN, SERVO4_PIN};
-bool servosDir[SERVO_AMOUNT] = {false, false, false, false};
+int servosStartupPos[SERVO_AMOUNT] = {180, 180, 180, 180}; // Позиция серв при старте перед тем
+bool servosDir[SERVO_AMOUNT] = {true, false, false, false};
 int servosPos[SERVO_AMOUNT] = {0, 0, 0, 0};
 int servosSpeed[SERVO_AMOUNT] = {90, 90, 90, 90};
 float servosAccel[SERVO_AMOUNT] = {0.3, 0.3, 0.3, 0.3};
-
-float* ikServosDeg = new float[SERVO_AMOUNT]{0};
 
 int robotState = 0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Setup");
+  Serial.println("Setup manipulator");
   for (byte i = 0; i < SERVO_AMOUNT; i++) {
-    servos[i].attach(10, SERVO_MIN_PULSE, SERVO_MAX_PULSE);
-    //servos[i].smoothStart();
-    servos[i].setSpeed(servosSpeed[i]);
-    servos[i].setAccel(servosAccel[i]);
-    servos[i].setMaxAngle(360);
-    servos[i].setDirection(servosDir[i]);
-    servos[i].setAutoDetach(true);
+    servos[i].attach(servosPins[i], SERVO_MIN_PULSE, SERVO_MAX_PULSE, servosStartupPos[i]);
+    servos[i].smoothStart(); // Смягчает движение серво из неизвестной позиции к стартовой. БЛОКИРУЮЩАЯ НА 1 СЕК!
+    servos[i].setSpeed(servosSpeed[i]); // Установить скорость
+    servos[i].setAccel(servosAccel[i]); // Установить ускорение
+    servos[i].setDirection(servosDir[i]); // Установить направление вращения
+    servos[i].setAutoDetach(true); // Автоотключение при достижении целевого угла (по умолчанию включено)
   }
-  // Установить на среднуюю позицию
-  //servos[0].setTarget(GetPulseWidthFromDeg(180));
-  servos[0].setTargetDeg(180);
-  //delay(1000);
-  Serial.println("Start");
-  tmr.setTime(1000);
+  Serial.println("Start work");
+  tmr.setTime(2000);
   tmr.start();
 }
 
@@ -54,7 +50,7 @@ float* Manipulator_IK(float x, float y, float z) {
   float a2 = 0;
   float a3 = 0;
   float *ik = new float[SERVO_AMOUNT - 1]{0};
-  ik[0] = 180 + degrees(a1), ik[1] = degrees(a2), ik[2] = degrees(a3);
+  ik[0] = 90 + degrees(a1), ik[1] = degrees(a2), ik[2] = degrees(a3);
   Serial.println("ik[0]: " + String(ik[0]));
   return ik;
 }
@@ -64,35 +60,40 @@ void loop() {
     servos[i].tick(); // Здесь происходит движение серво по встроенному таймеру!
     //servos[i].tickManual(); // Двигаем все сервы. Такой вариант эффективнее отдельных тиков
   }
-
+  if (tmrDebug.tick()) {
+    Serial.println(servos[0].getCurrentDeg());
+    if (servos[0].tick()) Serial.println("done");
+  }
+  
   if (tmr.tick() && true) {
+    float* ikServosDeg = new float[SERVO_AMOUNT]{0};
     Serial.println("Tmr finish");
+    tmr.setTime(2000);
     if (robotState == 0) {
       servosPos[0] = 0;
-      tmr.setTime(3000);
+      tmr.setTime(2000);
       robotState = 1;
     } else if (robotState == 1) {
       servosPos[0] = round(54.1);
-      tmr.setTime(3000);
+      tmr.setTime(2000);
       robotState = 2;
     } else if (robotState == 2) {
       ikServosDeg = Manipulator_IK(30, 50, 0);
-      servosPos[0] = ikServosDeg[0] * 1.4;
-      tmr.setTime(3000);
+      servosPos[0] = round(ikServosDeg[0]);
+      tmr.setTime(2000);
       robotState = 3;
     } else if (robotState == 3) {
       servosPos[0] = round(305.91);
-      tmr.setTime(3000);
+      tmr.setTime(2000);
       robotState = 4;
     } else if (robotState == 4) {
       servosPos[0] = 360;
-      tmr.setTime(1000);
+      tmr.setTime(2000);
       robotState = 0;
     }
-    Serial.println("servPos: " + String(servosPos[0]) + "\t" + "robotState: " + String(robotState));
-    //servos[0].setTarget(GetPulseWidthFromDeg(servosPos[0]));
+    Serial.println("serv1Pos: " + String(servosPos[0]) + "\t" + "robotState: " + String(robotState));
     servos[0].setTargetDeg(servosPos[0]);
-    //delete[] ikServosDeg; // Удалить память под массив из функции Manipulator_IK
+    delete[] ikServosDeg; // Удалить память под массив из функции Manipulator_IK
   }
 }
 
